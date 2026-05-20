1c91e6dfe621b4107546b31289eaeed09bbc46d3
difaoctaviafirdayani <difaoctaviafirdayani@exampel.com>
2026-05-21 04:39:44 +0700

    update terbaru

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
   },
 });
 
-// Inject token otomatis setiap request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -17,7 +16,6 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-// Handle error response secara global
 api.interceptors.response.use(
   (response) => response,
   (error) => {
